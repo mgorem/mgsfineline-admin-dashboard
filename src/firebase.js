cef423ff1502321ef06f890f6fc3d37912206f3c
mgorem <oremgitonga22@gmail.com>
2022-07-18 00:39:56 +0300

    initialise admin app

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.MGS_FINILINE_PRV_KEY,
+  authDomain: "mgsfineline.firebaseapp.com",
+  projectId: "mgsfineline",
+  storageBucket: "mgsfineline.appspot.com",
+  messagingSenderId: "794676245747",
+  appId: "1:794676245747:web:4b46381ddd35ca392e7386",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+import "./login.scss"
+
+const Login = () => {
+
+  const [error, setError] = useState(false)
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+  }
+
+  return (
+    <div className="login">
+      <form onSubmit={handleLogin}>
+        <input type="email" placeholder="email" />
+        <input type="password" placeholder="password" />
+        <button type="submit">Login</button>
+        {error && <span>Wrong email or password!</span>}
+      </form>
+    </div>
+  )
+}
+
+export default Login
